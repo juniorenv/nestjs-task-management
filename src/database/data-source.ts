341c5b20cf23d0d350586e2536a16ebcfec2e8f0
Junior <lightgraphics80@gmail.com>
2025-08-30 05:13:39 +0000

    feat(orm): integrate TypeORM entities into modules
    - Add TaskEntity and UserEntity to data-source entities
    - Import TypeOrmModule.forFeature in TaskModule
    - Import TypeOrmModule.forFeature in UserModule
    - Register entities for repository injection

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { TaskEntity } from './entities/task.entity';
 
 config();
 
@@ -13,7 +15,7 @@ const dataSourceOptions: DataSourceOptions = {
   database: configService.get<string>('DB_NAME'),
   port: +configService.getOrThrow<number>('DB_PORT'),
   password: configService.get<string>('DB_PASSWORD'),
-  entities: [],
+  entities: [UserEntity, TaskEntity],
   migrations: [__dirname + '/migrations/*.ts'],
   synchronize: false,
 };
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntity } from 'src/database/entities/task.entity';
 
 @Module({
   providers: [TaskService],
   controllers: [TaskController],
+  imports: [TypeOrmModule.forFeature([TaskEntity])],
 })
 export class TaskModule {}
